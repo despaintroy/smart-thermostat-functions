4d2a4921c765213dfd1512ca649694d0156fec0d
Troy DeSpain <despaintroy@gmail.com>
2021-10-06 23:57:43 -0600

    Make requests to OpenWeatherMap

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,34 @@
-import * as functions from "firebase-functions";
+import "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as cors from "cors";
+import express = require("express");
+import {Request, Response} from "express";
+import * as functions from "firebase-functions";
+import OpenWeatherMap from "openweathermap-ts";
+
+const openWeather = new OpenWeatherMap({
+  apiKey: functions.config().openweathermap.key,
+});
+
+const app = express();
+
+app.use(cors({origin: true}));
+
+app.get("/api/weather", (req: Request, res: Response) => {
+  openWeather
+      .getCurrentWeatherByZipcode(84341)
+      .then((r) =>
+        res.status(200).send({
+          feelsLike: r.main.feels_like,
+          temperature: r.main.temp,
+          humidity: r.main.humidity,
+        })
+      )
+      .catch(() =>
+        res.status(500).send({
+          message: "Issue fetching weather from OpenWeather",
+        })
+      );
+});
+
+exports.app = functions.https.onRequest(app);
